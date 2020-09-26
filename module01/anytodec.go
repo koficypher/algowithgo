aaa3cc4798567604b4d6c897eb4d7ec39d87a8f3
koficypher <skcypher6@gmail.com>
2020-09-26 23:55:39 +0000

    any number to any base

--- module01/anytodec.go
+++ module01/anytodec.go
@@ -7,11 +7,24 @@ import (
 // AnyToDec converts a binary digit to a base by
 func AnyToDec(value string, base int) int {
 	//	var result int
-	for _, num := range value {
-		//multiplier := base * *int(value[i])
-		//result = int(num) *
-		fmt.Printf("%d\n", num)
+	var charset = "0123456789ABCDEF"
+	res := 0
+	multiplier := 1
+	for i := len(value) - 1; i >= 0; i-- {
+		index := -1
+		for j, l := range charset {
+			if l == rune(value[i]) {
+				index = j
+				break
+			}
+		}
 
+		if index < 0 {
+			panic("something went wrong")
+		}
+		res = res + index*multiplier
+		multiplier = multiplier * base
 	}
-	return 0
+	fmt.Println(res)
+	return res
 }
